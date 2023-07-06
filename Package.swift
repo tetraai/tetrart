35d6917bd448cbd3a6f766b59d17ba17e198f7c5
Jeff Kilpatrick <random-g@bouncingsheep.org>
2023-07-06 14:35:18 -0700

    Initial commit with sample content

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TetraRT",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(name: "TetraRT", targets: ["TetraRT"])
+    ],
+    targets: [
+        .binaryTarget(name: "TetraRT",
+                      url: "https://tetra-biz-data-customer.s3.us-west-2.amazonaws.com/tetrart_binaries/apple/TetraRT.xcframework-0.1.0-with-dsyms.zip",
+                      checksum: "1f61fd6200dff5ac570de544807ce434032d1e5042fda227f234b0104d9a6b1b"),
+
+        .executableTarget(name: "SmokeTest", dependencies: [.byName(name: "TetraRT")])
+    ],
+    cxxLanguageStandard: .cxx17
+)
