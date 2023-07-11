e079eee989638262f7da321b622b73dfc8da19fc
Jeff Kilpatrick <jeff@tetra.ai>
2023-07-11 10:32:03 -0700

    Update Apple package to 0.1.1.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "TetraRT",
-                      url: "https://tetra-biz-data-customer.s3.us-west-2.amazonaws.com/tetrart_binaries/apple/TetraRT.xcframework-0.1.0-with-dsyms.zip",
-                      checksum: "1f61fd6200dff5ac570de544807ce434032d1e5042fda227f234b0104d9a6b1b")
+                      url: "https://tetra-biz-data-customer.s3.us-west-2.amazonaws.com/tetrart_binaries/apple/TetraRT.xcframework-0.1.1.zip",
+                      checksum: "65eaca9bb41d62f57c1ca107808de0bf70be8e86bb7180c252296504d6b5f846")
     ]
 )
