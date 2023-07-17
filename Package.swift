2505ccf5f777918903e199f0053b18835b263755
Jeff Kilpatrick <jeff@tetra.ai>
2023-07-17 15:41:04 -0700

    Update Apple package to 0.2.0.

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "TetraRT",
-                      url: "https://tetra-biz-data-customer.s3.us-west-2.amazonaws.com/tetrart_binaries/apple/TetraRT.xcframework-0.1.0-with-dsyms.zip",
+                      url: "https://tetra-release.s3.us-west-2.amazonaws.com/TetraRT/apple/TetraRT.xcframework-0.2.0.zip",
                       checksum: "1f61fd6200dff5ac570de544807ce434032d1e5042fda227f234b0104d9a6b1b"),
 
         .executableTarget(name: "SmokeTest", dependencies: [.byName(name: "TetraRT")])
